10376de58e4075e589117fc71ea41a751fc32da7
Betelhemf567 <b.chelebo@alustudent.com>
2025-02-21 01:42:26 -0800

    add file

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""0-subs.py"""
-
+""" top_ten.py """
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Reddit subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        return 0
-    else:
-        return response.json().get("data").get("subscribers")
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
